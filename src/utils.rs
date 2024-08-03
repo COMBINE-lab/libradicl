0d77748d919a6c8312a7fabef2c944d3de69a523
Rob Patro <rob@newton>
2024-08-03 00:33:13 -0400

    add read_chunk_header function to util

--- src/utils.rs
+++ src/utils.rs
@@ -10,6 +10,8 @@
 //! This module contains some utility constants and functions that are
 //! helpful in processing RAD information.
 
+use scroll::Pread;
+
 pub const MASK_TOP_BIT_U32: u32 = 0x7FFFFFFF;
 pub const MASK_LOWER_31_U32: u32 = 0x80000000;
 pub const SPLICE_MASK_U32: u32 = 0xFFFFFFFE;
@@ -25,4 +27,15 @@ pub const SPLICE_MASK_U32: u32 = 0xFFFFFFFE;
 #[inline]
 pub fn has_data_left<T: std::io::BufRead>(reader: &mut T) -> std::io::Result<bool> {
     reader.fill_buf().map(|b| !b.is_empty())
+}
+
+/// Reads the header of a chunk, returning the number of bytes and number of records.
+/// This function lives in util and outside of the [Chunk] trait because it is agnostic
+/// to the type of the chunk (i.e. the record type).
+pub fn read_chunk_header<T: std::io::BufRead>(reader: &mut T) -> std::io::Result<(u32, u32)> {
+    let mut buf = [0_u8; 8];
+    reader.read_exact(&mut buf)?;
+    let nbytes = buf.pread::<u32>(0).unwrap();
+    let nrec = buf.pread::<u32>(4).unwrap();
+    Ok((nbytes, nrec))
 }
